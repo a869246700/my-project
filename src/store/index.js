55fcfb5d54b483612d744b473a6d6a0dbe640d99
869246700@qq.com <869246700@qq.com>
2020-05-24 23:17:06 +0800

    优化vuex保存至sessionStorage

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,7 @@ import Vuex from 'vuex'
 import mutations from './mutations'
 import actions from './actions'
 import getters from './getters'
+import persistedState from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
@@ -19,5 +20,10 @@ export default new Vuex.Store({
   mutations,
   actions,
   getters,
-  modules
+  modules,
+  plugins: [
+    persistedState({
+      storage: window.sessionStorage
+    })
+  ]
 })
